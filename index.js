3be262890b4b8f0699b53e4066d8be502e451995
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2024-09-16 23:50:00 +0500

    fixed

--- index.js
+++ index.js
@@ -10,62 +10,58 @@ const bot = new TelegramBot(TOKEN, { polling: true });
 async function main() {
   const psql = await postgres();
 
-  await bot.onText(/^\/start$/, (message) => {
+  bot.onText(/^\/start$/, (message) => {
     bot.sendMessage(
       message.chat.id,
       `Botdan foydalanish uchun guruhga administrator sifatida qoʻshishingiz kerak!`
     );
   });
 
-  await bot.onText(/\/startSvoyak/, (message) => {
+  bot.onText(/\/startSvoyak/, (message) => {
     GameController(message.chat, message.from, "start", bot, psql);
   });
 
-  await bot.onText(/\/endSvoyak/, (message) => {
+  bot.onText(/\/endSvoyak/, (message) => {
     GameController(message.chat, message.from, "end", bot, psql);
   });
 
-  await bot.onText(/^[-+]?\d+?$/, (message) => {
+  bot.onText(/^[-+]?\d+?$/, (message) => {
     if (message.reply_to_message) {
       GamerController(message, bot, psql);
     }
   });
 
-  await bot.onText(/\/removeMe/, (message) => {
+  bot.onText(/\/removeMe/, (message) => {
     ExtraControllers.RemoveGamer(message, bot, psql);
   });
 
-  await bot.onText(/\/changeCreator/, (message) => {
+  bot.onText(/\/changeCreator/, (message) => {
     if (message.reply_to_message) {
       ExtraControllers.ChangeCreator(message, bot, psql);
     }
   });
 
-  await bot.onText(/\/getStats/, (message) => {
+  bot.onText(/\/getStats/, (message) => {
     ExtraControllers.StatsController(message, bot, psql);
   });
 
-  await bot.onText(/\/clearDB/, (message) => {
+  bot.onText(/\/clearDB/, (message) => {
     ExtraControllers.ClearDB(message, bot, psql);
   });
 
-  await bot.onText(/\/help/, (message) => {
+  bot.onText(/\/help/, (message) => {
     bot.sendMessage(
       message.chat.id,
       `Botdan foydalanish uchun buyruqlar:\n\n/startSvoyak - oʻyinni boshlash buyruqi. Ushbu buyruqni bergan foydalanuvchi oʻyin boshlovchisi hisoblanadi va ochko berish imkoniyatiga ega boʻladi. Ochkolar xabarga javob sifatida berilishi kerak.\n\n/changeCreator - boshlovchini oʻzgartirish buyruqi. Ushbu buyruqdan amaldagi boshlovchi yoki guruh adminlari foydalanishi mumkin. Buyruq xabarga javob sifatida berilishi kerak.\n\n/removeMe - tablodan oʻz ismingizni oʻchirish uchun ishlatishingiz mumkin.\n\n/endSvoyak - oʻyinni yakunlash va natijalarni e'lon qilish buyruqi. Ushbu buyruqdan amaldagi boshlovchi yoki guruh adminlari foydalanishi mumkin.`
     );
   });
 
-  await bot.onText(/\/about/, (message) => {
+  bot.onText(/\/about/, (message) => {
     bot.sendMessage(
       message.chat.id,
       `Shaxsiy oʻyin jarayonida ochkolarni hisoblab boruvchi bot.\nDasturchi: @dasturchining_tundaligi`
     );
   });
-
-  // await bot.onText(/\/aytibar/, (message) => {
-  //   ExtraControllers.Aytibar(message, bot);
-  // });
 }
 
 main();
